0f97a0c1b7e1c85b466c21e83fdab805319af2a8
Siddhartha Bharali <siddharthabharaliassam@gmail.com>
2025-02-15 09:07:56 +0530

    done

--- Task 1/client/src/serverURL.js
+++ Task 1/client/src/serverURL.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = "http://localhost:8000/api/users";
+//export const SERVER_URL = "http://localhost:8000/api/users";
+export const SERVER_URL = "https://heal-tether.onrender.com/api/users";
